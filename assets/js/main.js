6e6ce57ec5cf4daaaa67b68a1fdf2839f62b0a9b
Roccofranchini <rocco.franchini1@gmail.com>
2021-11-11 00:57:42 +0100

    skills-toggle-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,10 +31,27 @@ function linkAction() {
 	// When we click on each nav__link, we remove the show-menu class
 	navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+	let itemClass = this.parentNode.className;
+
+	for (let i = 0; i < skillsContent.length; i++) {
+		skillsContent[i].className = "skills__content skills__close";
+	}
+	if (itemClass === "skills__content skills__close") {
+		this.parentNode.className = "skills__content skills__open";
+	}
+}
+
+skillsHeader.forEach((el) => {
+	el.addEventListener("click", toggleSkills);
+});
 /*==================== QUALIFICATION TABS ====================*/
 
 /*==================== SERVICES MODAL ====================*/
